5f67a4e92088d9790f9fa39d5a683d9eabc7f819
Hafizh H Asyhari <81144031+hafizhhasyhari@users.noreply.github.com>
2024-05-19 16:31:39 +0700

    Create Materi-005-upperlower.py

---
+++ Materi-005-upperlower.py
@@ -0,0 +1,8 @@
+var = input("masukan satu kata! : ")
+kapital = var.upper()
+kecil = var.lower()
+print(f'contoh kata       = {var}')
+print(f'menggunakan upper = {kapital}')
+print(f'menggunakan lower = {kecil}')
+
+// Author by hafizhhasyhari //
