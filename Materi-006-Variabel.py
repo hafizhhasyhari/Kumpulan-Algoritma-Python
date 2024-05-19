e9f556fa251eb0c49fc12fb2b09671b728c7aa8e
Hafizh H Asyhari <81144031+hafizhhasyhari@users.noreply.github.com>
2024-05-19 16:33:04 +0700

    Create Materi-006-Variabel.py

---
+++ Materi-006-Variabel.py
@@ -0,0 +1,28 @@
+def main():
+    a = 13
+    b = 45.67
+    
+    print('Sebelum diubah:')
+    print('Nilai a\t\t:', a)
+    print('Nilai b\t\t:', b)
+    print('Tipe data a\t:', type(a))
+    print('Tipe data b\t:', type(b))
+    print('Identitas a\t:', id(a))
+    print('Identitas b\t:', id(b))
+    
+    #mengubah variabel a dan b untuk menunjuk objek lain
+    
+    a = 'Python'
+    b = True
+    
+    print('Setelah diubah:')
+    print('Nilai a\t\t:', a)
+    print('Nilai b\t\t:', b)
+    print('Tipe data a\t:', type(a))
+    print('Tipe data b\t:', type(b))
+    print('Identitas a\t:', id(a))
+    print('Identitas b\t:', id(b))
+if __name__ == '__main__':
+        main()
+
+// hafizhhasyhari //
