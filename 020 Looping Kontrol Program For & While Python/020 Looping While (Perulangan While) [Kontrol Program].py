b328a9954386567fea5761bf55cb0b51e9d53b25
Hafizh H Asyhari <81144031+hafizhhasyhari@users.noreply.github.com>
2024-05-26 23:37:06 +0700

    Create 020 Looping While (Perulangan While) [Kontrol Program].py

---
+++ 020 Looping Kontrol Program For & While Python/020 Looping While (Perulangan While) [Kontrol Program].py
@@ -0,0 +1,20 @@
+# Kontrol Program - While
+
+# Uncomment blok program contoh yang akan dijalankan!
+
+'''
+# Perulangan While - Berbatas
+kalimat=input("Masukkan data: ")
+print("\nTampilkan data 5 kali \n")
+a = 1
+while(a<=5):                # Pembatas jumlah perulangan
+    print(a,".",kalimat)
+    a=a+1
+'''
+
+'''
+# Perulangan While - Tak Berbatas
+while True:                 # Jumlah perulangan tidak ada batas
+    print("Selamat Belajar Python")
+    print("di Robonesia")
+'''
