4d38111be30dba8dd8a2027292f8ca36d41c308c
Hafizh H Asyhari <81144031+hafizhhasyhari@users.noreply.github.com>
2024-05-26 23:20:15 +0700

    Create 001 Input dan Output.py

---
+++ 001 Input & Output Python/001 Input dan Output.py
@@ -0,0 +1,44 @@
+// Input dan output python
+// hafizhhasyhari
+
+# Fungsi INPUT
+
+# Ketik nama sebagai input berupa string
+'''
+nama = input("Masukkan nama Anda: ")
+print(nama)
+'''
+
+# Jumlahkan 2 buah input berupa string
+'''
+dataIn1 = input("String1: ")
+dataIn2 = input("String2: ")
+hasil = dataIn1 + dataIn2
+print(hasil)
+'''
+
+# input yang diketik berupa string, bukan int atau float, maka akans error
+'''
+dataIn1 = input("Angka1: ")
+dataIn2 = input("Angka2: ")
+hasil = dataIn1 * dataIn2
+print(hasil)
+'''
+
+# Input yang diketik berupa string, bukan int atau float
+# Supaya tidak error, konversi dulu string menjadi int atau float
+"""
+dataIn1 = input("Angka1: ")
+dataIn2 = input("Angka2: ")
+hasil = int(dataIn1) + float(dataIn2)
+print("Hasil penjumlahan: ", hasil)
+"""
+
+# Fungsi OUTPUT
+"""
+print("Hallo, Sahabat Robonesia")       # Menampilkan string (Kata/kalimat)
+print('Apa kabar teman-teman?')
+a = 7; b = 8
+c = a * b
+print("Hasil perkalian adalah: ", c)    # Menampilkan hasil operasi aritmatika
+"""
