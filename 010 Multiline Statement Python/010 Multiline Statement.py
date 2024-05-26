0a04c5cea740a649e78d5a7a0b7e471ec67c9a09
Hafizh H Asyhari <81144031+hafizhhasyhari@users.noreply.github.com>
2024-05-26 23:22:53 +0700

    Create 010 Multiline Statement.py

---
+++ 010 Multiline Statement Python/010 Multiline Statement.py
@@ -0,0 +1,20 @@
+# Multiline Statement
+
+# Contoh kelanjutan baris implisit
+x = ('1' + '2' + '3' + '4' + '5' +
+    '6' + '7')
+
+# Contoh kelanjutan baris eksplisit
+y = '0' + '8' + '5' + '7' + '6' + '5' + '4' + '3' + '3' +\
+    '7' + '5' + '3'
+
+hari_kerja = ['Senin', 'Selasa', 'Rabu',
+              'Kamis', 'Jumat']
+
+akhir_pekan = {'Sabtu',
+               'Ahad'}
+
+print ('Nilai x adalah: ', x)
+print ('Nilai y adalah: ', y)
+print ('Hari kerja adalah: ', hari_kerja)
+print ('Akhir pekan adalah: ', akhir_pekan)
