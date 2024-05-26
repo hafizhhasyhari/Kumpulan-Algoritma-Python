ff97bd331681330ee88755e9f856751f28641e4e
Hafizh H Asyhari <81144031+hafizhhasyhari@users.noreply.github.com>
2024-05-26 23:51:46 +0700

    Create 026 Built-in Memanggil Modul Built-in Python.py

---
+++ 026 Build-in Python (Modul)/026 Built-in Memanggil Modul Built-in Python.py
@@ -0,0 +1,6 @@
+// Author by hafizhhasyhari
+# Memanggil modul built-in di dalam Python
+
+import math          # Memanggil modul built-in pada Python
+help(math)           # Melihat informasi atribut di dalam suatu modul
+
