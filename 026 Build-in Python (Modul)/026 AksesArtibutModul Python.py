18e15ba6e2b935d226c720dce569980ec379a9d6
Hafizh H Asyhari <81144031+hafizhhasyhari@users.noreply.github.com>
2024-05-26 23:55:32 +0700

    Create 026 AksesArtibutModul Python.py

---
+++ 026 Build-in Python (Modul)/026 AksesArtibutModul Python.py
@@ -0,0 +1,8 @@
+# Mengakses atribut pada suatu modul
+
+import sys          # Memanggil modul built-in sys
+
+a = sys.maxsize          # Mengakses atribut maxsize pada modul sys
+b = 2**63 - 1
+print("Nilai a = ",a)
+print("Nilai b = ",b)
