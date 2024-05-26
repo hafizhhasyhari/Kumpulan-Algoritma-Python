8e74aa452ddc2bb458fcc03594710bfe5c88b2f7
Hafizh H Asyhari <81144031+hafizhhasyhari@users.noreply.github.com>
2024-05-26 23:54:14 +0700

    Create 026 Circle - Modul Lingkaran.py

---
+++ 026 Build-in Python (Modul)/026 Circle (Lingkaran) Python/026 Circle - Modul Lingkaran.py
@@ -0,0 +1,11 @@
+# Modul Lingkaran
+
+phi = 3.14          # inisialisasi nilai phi
+
+def luas(r):        # fungsi luas lingkaran
+    L = phi*(r**2)  # rumus luas lingkaran
+    return L
+
+def keliling(r):    # fungsi keliling lingkaran
+    K = 2*phi*r     # rumus keliling lingkaran
+    return K
